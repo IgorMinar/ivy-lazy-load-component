c70087892328c52f61a0203b7b3f7895594f3607
Igor Minar <igor@angular.io>
2019-09-18 23:00:32 +0100

    fixup the template and setup the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ivy-lazy-load';
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private cfr: ComponentFactoryResolver) {}
+
+  async lazyLoad() {}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+git clean -f && git co -f .
+ng g c Lazy -m Lazy -t -s
+ng serve --port 4111
+const {LazyComponent} = await import('./lazy/lazy.component');
+this.vcr.createComponent(this.cfr.resolveComponentFactory(LazyComponent));
+*/
